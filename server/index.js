5cecb81fc2f1e5ea8556f32d364318a1798690b7
ShadabChoudhary <shaddychoudhary21@gmail.com>
2022-04-03 15:33:14 +0530

    deploying on heroku

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const web = require("./routes/web.js");
 const registerRoute = require("./routes/registerRoute.js");
 const order = require("./routes/orderRoutes.js");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || "8000";
@@ -28,6 +29,14 @@ Connectdb(username, password);
 app.use("/api", web);
 app.use("/api", registerRoute);
 app.use("/api", order);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join("client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
